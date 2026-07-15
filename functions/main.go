db17e7d3f186778d34ad31fdf7a754551694dd80
misitox37 <mateolgallegoss@gmail.com>
2026-07-14 21:50:11 -0500

    checkhealth/verifying that divide function with one variable error works in the logic of main()

--- functions/main.go
+++ functions/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"errors"
+)
 
 // FUNCIÓN CLÁSICA:
 
@@ -14,28 +17,61 @@ func product(a complex64, b complex64) complex64 { // NO RECOMENDADO
 
 // FUNCIÓN QUE DEVUELVE MÁS DE UN VALOR:
 
-func divider(a, b float64) (float64, float64) {
-	quotient := a / b
-	remainder := float64(int(a) % int(b))
+func divider(a, b float64) (float64, error) {
 
-	return quotient, remainder
+	if b == 0 { // ERRORES DEVUELTOS POR MÉTODOS FRECUENTEMENTE
+		fmt.Errorf("No se puede dividir por cero")
+		return 0, errors.New("No sé dividir por 0")
+  }
+  quotient := a / b
+	return quotient, nil
+}
+
+// FUNCIÓN QUE VARÍA EL NÚMERO DE ARGUMENTOS
+
+func printNames(names ...string){
+
+	for _, name := range names {
+		fmt.Println(name)
+	}
+}
+
+func counter() func int {
+	c := 0
+	func int(){
+		c++
+		return
+	}
 }
 
 func main() {
 
-	var q, r float64
+/*	var q, r float64
 	_, err := fmt.Scanln(&q, &r)
 	if err != nil {
 		fmt.Println("Error to read:", err)
 		return
+  }
+
+	qu, err2 := divider (q, r)
+
+	if err2 != nil {
+		fmt.Println(err2)
+		return
 	}
-	
 
-	qu, re := divider (q, r)
+	fmt.Print("Quotient: ", qu) */
 
-	fmt.Print("Quotient: ", qu)
-	fmt.Println(", Remainder: ", re)
+	var u1, u2, u3, u4 string
+	_, e := fmt.Scanln(&u1 ,&u2, &u3, &u4)
+  if e != nil{
+		fmt.Println("Error to read:", e)
+		return
+	}
 
-	
+	name := printNames(u1, u2, u3, u4)
+	fmt.Println(name)
+
+  counter()
 }
 
